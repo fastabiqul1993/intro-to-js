79ff4177f1f55fefbf6d9180532dfe14fee0fc0c
fastaman993 <fastabiqul.khoirot993@gmail.com>
2019-08-15 22:22:09 +0700

    fix task 2 tugas 3

--- Task03/02-summary.js
+++ Task03/02-summary.js
@@ -1,11 +1,27 @@
+function splitNum(arr) {
+  return arr.toString().split('')
+}
+
 function summary(input) {
   let total = 0
 
   input.map(function(num) {
-    total += num
+     return total += num
   })
 
-  return total
+  let splitTotal = splitNum(total)
+
+  do {
+    let result = 0
+
+    splitTotal.map(function(num) {
+      return result += parseInt(num)
+    })
+
+    splitTotal = splitNum(result)
+
+    return result
+  }  while(splitTotal.length > 1)
 }
 
-console.log(summary([5,2,3,1,2]));
+console.log(summary([5,2,3,1,2,2,5,6,7,10]));
